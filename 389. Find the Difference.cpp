2d1fde265af67dc270af1e08179a7b8f5e3bc122
nexus <727724eucy030@skcet.ac.in>
2026-07-01 16:22:57 +0530

    find the diff

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            if(mp[t[i]] == 0){
+                return t[i];
+            }
+            mp[t[i]]--;
+        }
+        return ' ';
+    }
+};
