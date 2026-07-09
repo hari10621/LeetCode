505f49821a1d99697995cd507e7fc74c2fbd5c07
nexus <727724eucy030@skcet.ac.in>
2026-07-09 12:07:37 +0530

    tribonnaci number

---
+++ 1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trib(int n,vector<int>& dp){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = trib(n-1,dp) + trib(n-2,dp) + trib(n-3,dp);
+    }
+    int tribonacci(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        vector<int> dp(n+1,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        return trib(n,dp);
+    }
+};
